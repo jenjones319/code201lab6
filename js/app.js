df22f0718bc506be47d813e495ee8a9649e98890
jenjones319 <jenjones605@gmail.com>
2020-06-23 11:05:26 -0500

    update current files

--- js/app.js
+++ js/app.js
@@ -1,41 +1,66 @@
 'use strict';
 console.log('app.js is connected.')
 
-/*//var education = ['a','b','c'];
-var personOne = {
+var hoursOperation = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ']; //finish the array
 
-    name: 'Jen Jones',
-    age: 45,
-    codingStudent: true,
-    education: ['USD', 'Kirkwood', 'DeltaV'],
+var storeOne = {
+    location: 'seattle',
+    minCust: 23,
+    maxCust: 65, 
+    avgPerCust: 6.3, 
+    totalHourlyCookies: 0, 
+    customerPerHour: [],
+    cookiesPerHour: [],
 
-    code: function(){
-        console.log('The floor is lava.');
-
-        return this.education[Math.floor(Math.random() * this.education.length)];
-    }
-
-};
-
----- */
-
-var petOne = {
-    name: 'Tom',
-    breed: 'Tabby',
-    age: 0,
-    imageName: 'tomCat'
-    isGoodKids: true,
-    isGoodDogs: false,
-    isGoodAnimal: true,
-    setAge:  function(){
-        this.age = randomAge(3, 12) + ' Months';
-    }
-};
-
-function randomAge(min, max) {
-    return Math.floor(Math.random() * max - min + 1) + min);
+calcCustomersPerHour : function() {
+    for(var i = 0; i < hoursOperation.length; i++){
+        this.customerPerHour.push(randomCustomerNumbers(this.minCust,this.maxCust));
 }
 
-petOne.isFixed = true;
+},
+calcCookiesPerHour: function(){
+    this.calcCustomersPerHour();
+    for(var 1=0; i < hoursOperation.length; i++){
+        var oneHour = Math.ceil(this.customerPerHour[i] * this.avgPerCust);
+        }
+    },
+};
 
-var parentElement = document.getElementById('kittenProfiles');
+var allShops = [StoreOne];
+function renderAllShops(){
+    for(var i=0; i < )
+
+
+
+function Store(location, minCust, maxCust, avgPerCust) {
+    this.location = location;
+    this.minCust = minCust;
+    this.maxCust = maxCust;
+    this.avgPerCust = avgPerCust;
+this.Store = function() {
+    return this.maxCust - this.minCust;
+    };
+}
+
+var seattleStore = new Store('Seattle', 23, 65, 6.3);
+var tokyoStore = new Store('Tokyo', 3, 24, 1.2);
+var dubaiStore = new Store('Dubai', 11, 38, 3.7);
+var parisStore = new Store('Paris', 20, 38, 2.3);
+var limaStore = new Store('Lima', 2, 16, 4.6);
+
+var detailsStore = (seattleStore.location + ' Min. Customers: ' + minCust + ' Max. Customers: ' + maxCust + 'Avg. Sale Per: ' + avgPerCust); 
+
+
+var unorderedList = document.getElementById('seattle');
+    for(var i = 0; i < hoursOperation.length i++){
+        var listItems = document.create element('li');
+        listItems.textContent = hoursOperation[i] + this.cookiesPerHour[i] + 'cookies'.;
+        unorderedList.appendChild(listItems);
+    }
+    listItems = document.createElement('li');
+    listItems.textContent = 'Total:' + this.totalHourlyCookies + 'cookies.';
+    unorderedList.appendChild(listItems);
+}
+
+var allShops = [];
+
